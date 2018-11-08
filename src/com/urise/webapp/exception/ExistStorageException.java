3f8edd16e5a2de0d93eb9221a879af2b4446bd76
Eldar Akavov <desert73eagle@gmail.com>
2018-11-08 12:26:00 +0300

    created new directory Test and new class MainReflection

--- src/com/urise/webapp/exception/ExistStorageException.java
+++ src/com/urise/webapp/exception/ExistStorageException.java
@@ -2,6 +2,6 @@ package com.urise.webapp.exception;
 
 public class ExistStorageException extends StorageException {
     public ExistStorageException(String uuid) {
-        super(uuid);
+        super("Resume " + uuid + " already exist", uuid);
     }
 }
